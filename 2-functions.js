e7a204e486cfb664b77db4500b9908999291e8b3
muhammadfahid12 <ranafahadirshad@gmail.com>
2024-03-17 16:22:55 +0500

    changes in functions code

--- 2-functions.js
+++ 2-functions.js
@@ -1,5 +1,4 @@
-let numbers = [1, 2, 3, 4];
-let squared = numbers.map(function(num) {
-  console.log(num * num);
-});
+let myStr = "This is the frist sentence.";
+myStr+=" This is the second sentence." 
 
+console.log(myStr);
